4cfbeba4fa16312d8b897a4aaa433a90ab360cf8
Nehemias Nestmann <nehemias@bombieri.com.ar>
2020-04-24 12:02:54 -0300

    modiff ipn

--- ipn.php
+++ ipn.php
@@ -1,3 +1,40 @@
 <?php 
 
-header("HTTP/1.1 200 OK");
+require __DIR__  . '/vendor/autoload.php';
+
+MercadoPago\SDK::setAccessToken("YOUR_ACCESS_TOKEN");
+
+switch($_POST["type"]) {
+    case "payment":
+        $payment = MercadoPago\Payment::find_by_id($_POST["id"]);
+        if($payment){
+            header("HTTP/1.1 200 OK");
+        } else {
+            header("HTTP/1.1 201 ERROR");
+        }
+        break;
+    case "plan":
+        $plan = MercadoPago\Plan::find_by_id($_POST["id"]);
+        if($plan){
+            header("HTTP/1.1 200 OK");
+        } else {
+            header("HTTP/1.1 201 ERROR");
+        }
+        break;
+    case "subscription":
+        $plan = MercadoPago\Subscription::find_by_id($_POST["id"]);
+        if($plan){
+            header("HTTP/1.1 200 OK");
+        } else {
+            header("HTTP/1.1 201 ERROR");
+        }
+        break;
+    case "invoice":
+        $plan = MercadoPago\Invoice::find_by_id($_POST["id"]);
+        if($plan){
+            header("HTTP/1.1 200 OK");
+        } else {
+            header("HTTP/1.1 201 ERROR");
+        }
+        break;
+}
